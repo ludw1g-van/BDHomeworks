992eeaf102bc7162bfd4cd235d7f1a4944faf0f8
ludovicodimartino <ludovico.dimartino@studenti.unitn.it>
2024-03-20 18:03:22 +0100

    add initial structure of the code - proposal
    Struttura iniziale del codice con una sola classe. Ho alcuni dubbi:
    - Va bene fare tutto in una classe? Perchè altrimenti vanno più classi nello stesso file che non è proprio il massimo
    - Non ho ben capito quanti round vanno fatti nello step A dell'algoritmo che approssima

---
+++ HW1/src/G039HW1.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class G039HW1{
+
+
+    public static void main(String[] args) throws IOException {
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+        // CHECKING NUMBER OF CMD LINE PARAMETERS
+        // Parameters are: num_partitions, <path_to_file>
+        // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+        if (args.length != 5) {
+            throw new IllegalArgumentException("USAGE: file_path D M K L");
+        }
+
+        // Parameters parsing
+
+    }
+
+    public static void ExactOutliers(ArrayList<Point> points){
+        // Double for loop
+    }
+
+    public static void MRApproxOutliers(JavaRDD<Point> points, float D, int M, int K){
+        // Step A: Transform input RDD into an RDD of non-empty cells with their point counts
+        // Step B: Compute N3 and N7 for each cell
+    }
+}
